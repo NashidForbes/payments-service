06d6115834f8c0972dc3d31362e4b0d48bedb06a
NashidForbes <NashidForbes@biohumans.com>
2023-09-19 01:34:12 -0400

    Adding logic to handle the entity id for payment id not being set.

--- src/main/java/io/axoniq/paymentsservice/coreapi/data/domain/PaymentEntity.java
+++ src/main/java/io/axoniq/paymentsservice/coreapi/data/domain/PaymentEntity.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "payments")
 public class PaymentEntity {
+    private static final long serialVersionUID = 5313493413859894898L;
+
     @Id
     private String paymentId;
     @Column
--- src/main/java/io/axoniq/paymentsservice/query/handler/PaymentEventsHandler.java
+++ src/main/java/io/axoniq/paymentsservice/query/handler/PaymentEventsHandler.java
@@ -3,11 +3,15 @@ package io.axoniq.paymentsservice.query.handler;
 import io.axoniq.core.events.PaymentProcessedEvent;
 import io.axoniq.paymentsservice.coreapi.data.domain.PaymentEntity;
 import io.axoniq.paymentsservice.coreapi.data.domain.interfaces.PaymentsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
+@Slf4j
 public class PaymentEventsHandler {
 
     private final PaymentsRepository paymentsRepository;
@@ -20,6 +24,11 @@ public class PaymentEventsHandler {
     public void on(PaymentProcessedEvent event) {
         PaymentEntity paymentEntity = new PaymentEntity();
         BeanUtils.copyProperties(event, paymentEntity);
+
+        if(event.getPaymentId().isBlank()) {
+            paymentEntity.setPaymentId(UUID.randomUUID().toString());
+            log.info("No payment id provided by PaymentProcessedEvent event field paymentId, generating a new one");
+        }
 
         try {
             paymentsRepository.save(paymentEntity);
