56c528ef06c9df63cea168b2b85e0423535870d9
NashidForbes <NashidForbes@biohumans.com>
2023-10-01 12:12:53 -0400

    Spring Cloud Security enhancements to be able to access a secured (http login) Security Cloud Config server.

--- src/main/java/io/axoniq/paymentsservice/config/rest/RestTemplate.java
+++ src/main/java/io/axoniq/paymentsservice/config/rest/RestTemplate.java
@@ -1,4 +1,4 @@
-package io.axoniq.paymentsservice.query.rest;
+package io.axoniq.paymentsservice.config.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
